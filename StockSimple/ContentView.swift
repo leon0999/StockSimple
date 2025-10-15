fcb6a2d3200a223f8fc1d5659ed14ef1585a0b0b
Leon <leon0999@gmail.com>
2025-10-15 16:16:40 +0900

    Initial Commit

---
+++ StockSimple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockSimple
+//
+//  Created by Justee on 10/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockSimple/StockSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSimpleApp.swift
+//  StockSimple
+//
+//  Created by Justee on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct StockSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
